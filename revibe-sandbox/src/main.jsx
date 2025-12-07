a4096f1651c47ae857954f28d197fe4e9cdfd05c
Julian Zienert <jzienert@student.codam.nl>
2025-12-07 01:14:28 +0100

    Restore WebSocket Logic in Lobby: Use shared WebSocketProvider for Auth and Room Listing

--- revibe-sandbox/src/main.jsx
+++ revibe-sandbox/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { WebSocketProvider } from './contexts/WebSocketProvider.jsx';
 import './index.css'
 import App from './App.jsx'
 import { Lobby } from './components/Lobby.jsx'
@@ -12,10 +13,12 @@ createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <BrowserRouter>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <Routes>
-            <Route path="/room/:roomId" element={<App />} />
-            <Route path="/" element={<Lobby />} />
-          </Routes>
+          <WebSocketProvider>
+            <Routes>
+              <Route path="/room/:roomId" element={<App />} />
+              <Route path="/" element={<Lobby />} />
+            </Routes>
+          </WebSocketProvider>
         </GoogleOAuthProvider>
       </BrowserRouter>
     </ErrorBoundary>
